15396bef0392e57307e3889f95c37b789cbb3eff
Kev5418 <kiyuquilema@uce.edu.ec>
2026-06-26 10:40:56 -0500

    crear estructura inicial del proyecto

---
+++ app/src/main/java/com/yuquilema/multi_timerfood/Usuario.java
@@ -0,0 +1,57 @@
+package com.yuquilema.multi_timerfood;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+@Entity(tableName = "usuarios")
+public class Usuario {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String nombre;
+    private String correo;
+    private String password;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, String correo, String password) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ app/src/main/java/com/yuquilema/multi_timerfood/UsuarioDao.java
@@ -0,0 +1,19 @@
+package com.yuquilema.multi_timerfood;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+@Dao
+public interface UsuarioDao {
+
+    @Insert
+    void insertarUsuario(Usuario usuario);
+
+    @Query("SELECT * FROM usuarios WHERE correo = :correo LIMIT 1")
+    Usuario buscarPorCorreo(String correo);
+
+    @Query("SELECT * FROM usuarios WHERE correo = :correo AND password = :password LIMIT 1")
+    Usuario login(String correo, String password);
+
+}
